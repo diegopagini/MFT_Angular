58e3d8192f5e0a4140ddca6bf911b7edd8ef0210
unknown <diegopagini777@gmail.com>
2020-11-27 09:42:11 -0300

    quedan cosas por ver para que funcione

--- src/app/field/field.component.ts
+++ src/app/field/field.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-field',
   templateUrl: './field.component.html',
-  styleUrls: ['./field.component.css']
+  styleUrls: ['./field.component.css'],
 })
 export class FieldComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/players/players.component.ts
+++ src/app/players/players.component.ts
@@ -7,40 +7,28 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./players.component.css'],
 })
 export class PlayersComponent implements OnInit {
-
   display: boolean = false;
-
   showDialog() {
     this.display = true;
   }
 
   players: Array<any> = [];
-  positions = [1 ,2 ,3 ,4, 5, 6, 7, 8, 9, 10, 11];
-  foot = ['left-footed', 'rigth-footed']
+  positions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+  foot = ['left-footed', 'rigth-footed'];
 
   team = new FormGroup({
     name: new FormControl('', [Validators.required]),
     lastName: new FormControl('', [Validators.required]),
-    position: new FormControl(''),
-    // position: new FormGroup({
-    //   1: new FormControl(1),
-    //   2: new FormControl(2),
-    //   3: new FormControl(3),
-    // }),
+    position: new FormControl('', [Validators.required]),
     weight: new FormControl('', [Validators.required]),
     height: new FormControl('', [Validators.required]),
     nationality: new FormControl('', [Validators.required]),
-    leftFooted: new FormControl(''),
+    leftFooted: new FormControl('', [Validators.required]),
   });
 
   constructor() {}
 
-  ngOnInit(): void {
-    this.team.controls.name.valueChanges.subscribe(selectedValue => {
-      this.filterCharacter()
-      console.log(selectedValue)
-    })
-  }
+  ngOnInit(): void {}
 
   get nameField() {
     return this.team.get('name');
@@ -70,20 +58,43 @@ export class PlayersComponent implements OnInit {
     return this.team.get('leftFooted');
   }
 
+  onClick() {
+    this.addPlayerToTheTeam();
+    this.filterCharacter();
+  }
+
   addPlayerToTheTeam() {
     if (this.team.valid) {
       this.players.push(this.team.value);
-      console.log(this.team.value);
     }
-    this.team.reset()
+    this.team.reset();
   }
 
   filterCharacter() {
-    this.players.filter(el => {
-      if (el.name == 'diego') {
-        document.getElementById('one').style.backgroundColor = "red"
+    this.players.filter((el) => {
+      if (el.position === 1) {
+        document.getElementById('one').style.backgroundColor = 'red';
+      } else if (el.position === 2) {
+        document.getElementById('two').style.backgroundColor = 'red';
+      } else if (el.position === 3) {
+        document.getElementById('three').style.backgroundColor = 'red';
+      } else if (el.position === 4) {
+        document.getElementById('four').style.backgroundColor = 'red';
+      } else if (el.position === 5) {
+        document.getElementById('five').style.backgroundColor = 'red';
+      } else if (el.position === 6) {
+        document.getElementById('six').style.backgroundColor = 'red';
+      } else if (el.position === 7) {
+        document.getElementById('seven').style.backgroundColor = 'red';
+      } else if (el.position === 8) {
+        document.getElementById('eight').style.backgroundColor = 'red';
+      } else if (el.position === 9) {
+        document.getElementById('nine').style.backgroundColor = 'red';
+      } else if (el.position === 10) {
+        document.getElementById('ten').style.backgroundColor = 'red';
+      } else if (el.position === 11) {
+        document.getElementById('eleven').style.backgroundColor = 'red';
       }
-    })
+    });
   }
-
 }
